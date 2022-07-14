8b0459c59c724126537b9b37566b3ed2f5b0aab7
Diego <34724728+Calcoph@users.noreply.github.com>
2022-07-15 01:02:16 +0200

    create main function

--- main.py
+++ main.py
@@ -116,118 +116,122 @@ hexpat definition:
     string += "super().__init__(_thisbitfield_s_name, _dollar___offset_copy, _dollar___offset.copy())\n"
     return string
 
-with open("ignore/testpath.txt", "r") as f:
-    file_path = f.readlines()[0]
+def translate_file(input_file_path: str, output_file_path):
+    with open(input_file_path, "r") as f:
+        lines = f.readlines()
 
-with open(file_path, "r") as f:
-    lines = f.readlines()
-
-padding_count = 0
-final_string = "from primitives import Dollar, Struct, u8, u16, u32, u64, u128, s8, s16, s32, s64, s128, Float, double, char, char16, Bool, Padding, BitField, sizeof, addressof\n\n"
-struct_name = ""
-bitfield_name = ""
-docstring = ""
-type_names = primitives.struct_names
-current_attribs = []
-attribs = []
-indentation_count = 0
-for line in lines:
-    if struct_name == "" and bitfield_name == "":
-        words = line.split(" ")
-        if words[0] == "struct":
-            docstring = line
-            final_string += "\n"
-            struct_name = list(words[1])
-            if struct_name[-1] == "\n":
-                struct_name = struct_name[:-1]
-            if struct_name[-1] == "{":
-                struct_name = struct_name[:-1]
-            struct_name = "".join(struct_name)
-            type_names.append(struct_name)
-        elif words[0] == "bitfield":
-            docstring = line
-            final_string += "\n"
-            bitfield_name = list(words[1])
-            if bitfield_name[-1] == "\n":
-                bitfield_name = bitfield_name[:-1]
-            if bitfield_name[-1] == "{":
-                bitfield_name = bitfield_name[:-1]
-            bitfield_name = "".join(bitfield_name)
-            type_names.append(bitfield_name)
-
-    elif struct_name != "":
-        docstring += line
-        if line[0] == "}":
-            final_string += make_struct(struct_name, attribs, docstring)
-            attribs = []
-            current_attribs = []
-            struct_name = ""
-            docstring = ""
-        else:
-            line = line.lstrip()
-            line = line.rstrip()
-            line = line.split(";")[0]
-            line = line.replace("$", "_dollar___offset")
+    padding_count = 0
+    final_string = "from primitives import Dollar, Struct, u8, u16, u32, u64, u128, s8, s16, s32, s64, s128, Float, double, char, char16, Bool, Padding, BitField, sizeof, addressof\n\n"
+    struct_name = ""
+    bitfield_name = ""
+    docstring = ""
+    type_names = primitives.struct_names
+    current_attribs = []
+    attribs = []
+    indentation_count = 0
+    for line in lines:
+        if struct_name == "" and bitfield_name == "":
             words = line.split(" ")
-            try_padding = words[0].split("[")
-            if try_padding[0] == "padding":
-                length = try_padding[1]
-                i = 2
-                while "]" not in try_padding[1]:
-                    length += words[i]
-                    i += 1
-                length = length.split("]")[0]
-                length = f"eval('{length}')"
-                pad_name = f"padding_{padding_count}"
-                padding_count += 1
-                attribs.append(("Padding", pad_name, length, indentation_count))
-            elif words[0] in type_names:
-                class_name = words[0]
-                att_name = words[1]
-                if "[" in att_name:
-                    (att_name, length) = att_name.split("[")
+            if words[0] == "struct":
+                docstring = line
+                final_string += "\n"
+                struct_name = list(words[1])
+                if struct_name[-1] == "\n":
+                    struct_name = struct_name[:-1]
+                if struct_name[-1] == "{":
+                    struct_name = struct_name[:-1]
+                struct_name = "".join(struct_name)
+                type_names.append(struct_name)
+            elif words[0] == "bitfield":
+                docstring = line
+                final_string += "\n"
+                bitfield_name = list(words[1])
+                if bitfield_name[-1] == "\n":
+                    bitfield_name = bitfield_name[:-1]
+                if bitfield_name[-1] == "{":
+                    bitfield_name = bitfield_name[:-1]
+                bitfield_name = "".join(bitfield_name)
+                type_names.append(bitfield_name)
+
+        elif struct_name != "":
+            docstring += line
+            if line[0] == "}":
+                final_string += make_struct(struct_name, attribs, docstring)
+                attribs = []
+                current_attribs = []
+                struct_name = ""
+                docstring = ""
+            else:
+                line = line.lstrip()
+                line = line.rstrip()
+                line = line.split(";")[0]
+                line = line.replace("$", "_dollar___offset")
+                words = line.split(" ")
+                try_padding = words[0].split("[")
+                if try_padding[0] == "padding":
+                    length = try_padding[1]
                     i = 2
-                    while "]" not in length:
-                        length += " " + words[i]
+                    while "]" not in try_padding[1]:
+                        length += words[i]
                         i += 1
                     length = length.split("]")[0]
                     length = f"eval('{length}')"
-                    attribs.append((class_name, att_name, length, indentation_count))
-                    current_attribs.append(att_name)
+                    pad_name = f"padding_{padding_count}"
+                    padding_count += 1
+                    attribs.append(("Padding", pad_name, length, indentation_count))
+                elif words[0] in type_names:
+                    class_name = words[0]
+                    att_name = words[1]
+                    if "[" in att_name:
+                        (att_name, length) = att_name.split("[")
+                        i = 2
+                        while "]" not in length:
+                            length += " " + words[i]
+                            i += 1
+                        length = length.split("]")[0]
+                        length = f"eval('{length}')"
+                        attribs.append((class_name, att_name, length, indentation_count))
+                        current_attribs.append(att_name)
+                    else:
+                        attribs.append((class_name, att_name, 0, indentation_count))
+                        current_attribs.append(att_name)
                 else:
-                    attribs.append((class_name, att_name, 0, indentation_count))
-                    current_attribs.append(att_name)
+                    if "}" in line:
+                        indentation_count -= 1
+                        line = line.replace("}", "")
+                    cur_indent = indentation_count
+                    line += "\n"
+                    if "{" in line:
+                        indentation_count += 1
+                        line = line.replace(" {", ":")
+                        line = line.replace("{", ":")
+                    line = line.replace("//", "#")
+                    attribs.append((plain_text,
+                                    line,
+                                    0,
+                                    cur_indent
+                                ))
+
+        elif bitfield_name != "":
+            docstring += line
+            if line[0] == "}":
+                final_string += make_bitfield(bitfield_name, attribs, docstring)
+                attribs = []
+                current_attribs = []
+                bitfield_name = ""
+                docstring = ""
             else:
-                if "}" in line:
-                    indentation_count -= 1
-                    line = line.replace("}", "")
-                cur_indent = indentation_count
-                line += "\n"
-                if "{" in line:
-                    indentation_count += 1
-                    line = line.replace(" {", ":")
-                    line = line.replace("{", ":")
-                line = line.replace("//", "#")
-                attribs.append((plain_text,
-                                line,
-                                0,
-                                cur_indent
-                              ))
+                line = line.lstrip()
+                line = line.rstrip()
+                line = line.split(";")[0]
+                words = line.split(": ")
+                attribs.append((words[0], int(words[1])))
 
-    elif bitfield_name != "":
-        docstring += line
-        if line[0] == "}":
-            final_string += make_bitfield(bitfield_name, attribs, docstring)
-            attribs = []
-            current_attribs = []
-            bitfield_name = ""
-            docstring = ""
-        else:
-            line = line.lstrip()
-            line = line.rstrip()
-            line = line.split(";")[0]
-            words = line.split(": ")
-            attribs.append((words[0], int(words[1])))
+    with open(output_file_path, "w") as f:
+        f.write(final_string)
 
-with open("ignore/test.py", "w") as f:
-    f.write(final_string)
+if __name__ == "__main__":
+    input_file_path = ""
+    output_file_path = ""
+
+    translate_file(input_file_path, output_file_path)
