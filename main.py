db2932d3976580b87fba080e6ab6e9bae2c26759
Diego <34724728+Calcoph@users.noreply.github.com>
2022-07-19 17:39:24 +0200

    fix error message in @ operator

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ hexpat definition:
 
 {indentation}def __matmul__(self, _dollar___offset):
 {indentation}{indentation}if not isinstance(_dollar___offset, Dollar):
-{indentation}{indentation}{indentation}raise Exception(f'An object of class "Dollar" must be used with the "@" operator. {{type(_dollar___offset_copy)}} was used instead')
+{indentation}{indentation}{indentation}raise Exception(f'An object of class "Dollar" must be used with the "@" operator. {{type(_dollar___offset)}} was used instead')
 {indentation}{indentation}_dollar___offset_copy = _dollar___offset.copy()
 """
     for (class_name, att_name, array_length, indentation_count) in attributes:
@@ -96,7 +96,7 @@ hexpat definition:
 
 {indentation}def __matmul__(self, _dollar___offset):
 {indentation}{indentation}if not isinstance(_dollar___offset, Dollar):
-{indentation}{indentation}{indentation}raise Exception(f'An object of class "Dollar" must be used with the "@" operator. {{type(_dollar___offset_copy)}} was used instead')
+{indentation}{indentation}{indentation}raise Exception(f'An object of class "Dollar" must be used with the "@" operator. {{type(_dollar___offset)}} was used instead')
 {indentation}{indentation}_dollar___offset_copy = _dollar___offset.copy()
 {indentation}{indentation}cur_byte = _dollar___offset.read(1)[0]
 """
